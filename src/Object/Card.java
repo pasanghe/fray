9d0a09255fed5ace3fc0d5b41b68e319d76ba007
pasanghe <pasanghe@sheridancollege.ca>
2019-03-08 18:38:55 -0500

    starter code for a reference

---
+++ src/Object/Card.java
@@ -0,0 +1,30 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date!
+ */
+package Object;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
+ *
+ * @author Sang Heon, Park (991517263)
+ * @author Gagandeep, Ghotra (9915284)
+ * @author Zain Razvi (991517290)
+ * @author Lee Fyffe (991540454)
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a
+     * regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
---
+++ src/Object/Player.java
@@ -0,0 +1,49 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date!
+ */
+package Object;
+
+/**
+ * A class that models each Player in the game. Players have an identifier,
+ * which should be unique.
+ *
+ * @author Sang Heon, Park (991517263)
+ */
+public abstract class Player {
+
+    private String name; //the unique ID for this player
+
+    /**
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the playerID
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Ensure that the playerID is unique
+     *
+     * @param givenID the playerID to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The method to be instantiated when you subclass the Player class with
+     * your specific type of Player and filled in with logic to play your game.
+     */
+    public abstract void play();
+
+}
